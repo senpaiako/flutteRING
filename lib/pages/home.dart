9ff81cd088e2ff2e04261081ea3f3819e7e0a7c1
Vladymir <vlady_noel789@gmail.com>
2024-09-23 13:39:45 +0800

    with timer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // Needed for Timer
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,70 +10,258 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Map data = {};
+  Timer? _timer;
+  String? clockInTime;
+  String? clockOutTime;
+  bool isClockedIn = false; // Track clock-in status
 
-  //set background
+  void toggleClock() {
+    final now = DateTime.now();
+    setState(() {
+      if (!isClockedIn) {
+        // Clock In
+        clockInTime = '${now.hour}:${now.minute.toString().padLeft(2, '0')}';
+      } else {
+        // Clock Out
+        clockOutTime = '${now.hour}:${now.minute.toString().padLeft(2, '0')}';
+      }
+      isClockedIn = !isClockedIn; // Toggle clock-in status
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startClock(); // Start the clock when the widget is initialized
+  }
+
+  void startClock() {
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      setState(() {
+        data['time'] = _getTimeString(); // Update the time
+      });
+    });
+  }
+
+  String _getTimeString() {
+    return DateTime.now().toLocal().toString().split(' ')[1].substring(0, 8);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // Cancel the timer when the widget is disposed
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Check if the arguments are not null and cast it to a Map
     final routeData = ModalRoute.of(context)?.settings.arguments;
     if (routeData != null && routeData is Map && data.isEmpty) {
       data = routeData;
-      print(data);
     }
-    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
-    Color bgColor = data['isDayTime'] ? Colors.white : Colors.indigo;
+
+    data['time'] = data['time'] ?? _getTimeString();
+    Color bgColor = data['isDayTime'] ? Colors.blue[300]! : Colors.indigo[900]!;
+
     return Scaffold(
       backgroundColor: bgColor,
       body: SafeArea(
         child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage('assets/$bgImage'),
-          )),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.lightBlueAccent,
+                Colors.white,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextButton.icon(
-                  onPressed: () async {
-                    dynamic result =
-                        await Navigator.pushNamed(context, '/location');
-                    setState(() {
-                      data = {
-                        'location': result['location'],
-                        'flag': result['flag'],
-                        'time': result['time'],
-                        'isDayTime': result['isDayTime']
-                      };
-                    });
-                  },
-                  icon: Icon(
-                    Icons.location_on,
-                    color: Colors.white,
-                  ),
-                  label: Text(
-                    'Edit Location',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                const Row(
                   children: [
-                    Text(
-                      data['location'],
-                      style: const TextStyle(
-                          fontSize: 20, letterSpacing: 2, color: Colors.white),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/profile.jpg'),
                     ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Babae Girl',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          'Head Marketing Queen',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    )
                   ],
                 ),
                 const SizedBox(height: 10),
-                Text(
-                  data['time'],
-                  style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+                Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(143, 158, 158, 158),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Today: ${data['time'] ?? _getTimeString()}',
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontFamily: 'Courier',
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () async {
+                                dynamic result = await Navigator.pushNamed(
+                                    context, '/location');
+                                setState(() {
+                                  data = {
+                                    'location': result['location'],
+                                    'flag': result['flag'],
+                                    'time': result['time'],
+                                    'isDayTime': result['isDayTime']
+                                  };
+                                });
+                              },
+                              icon: Icon(
+                                Icons.location_on,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      // CLOCK IN | CLOCK OUT
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: isClockedIn
+                                    ? Colors.green
+                                    : const Color.fromARGB(
+                                        110, 76, 175, 79), // Background color
+                                borderRadius:
+                                    BorderRadius.circular(5), // Rounded edges
+                              ),
+                              child: Column(
+                                children: [
+                                  const Row(
+                                    children: [
+                                      Icon(
+                                        Icons.access_time,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        size: 30,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        'Clock In',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontFamily: 'Courier',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    clockInTime != null ? '$clockInTime' : '--',
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontFamily: 'Courier',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          //clock out
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: isClockedIn
+                                    ? Color.fromARGB(139, 244, 67, 54)
+                                    : Colors.red, // Background color
+                                borderRadius:
+                                    BorderRadius.circular(5), // Rounded edges
+                              ),
+                              child: Column(
+                                children: [
+                                  const Row(
+                                    children: [
+                                      Icon(
+                                        Icons.access_time,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        size: 30,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        'Clock Out',
+                                        style: TextStyle(
+                                          fontSize: 19,
+                                          color: Colors.white,
+                                          fontFamily: 'Courier',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    clockOutTime != null
+                                        ? ' $clockOutTime'
+                                        : ' --',
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontFamily: 'Courier',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Expanded(
+                        child: Container(
+                          child: ElevatedButton(
+                            onPressed: toggleClock,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: isClockedIn
+                                  ? Colors.red
+                                  : Colors.green, // Button color
+                            ),
+                            child: Text(isClockedIn ? 'Clock Out' : 'Clock In'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
